f3113fe57eb9f7272d87fee63f495b537c0952db
udin201 <m.ridzqi.b@gmail.com>
2026-02-25 14:43:32 +0700

    feat: add linked list and queue implementations with basic operations and case study

---
+++ Pertemuan 4/Praktikum-4-kasus.py
@@ -0,0 +1,18 @@
+#====================================
+#Nama   : Muhammad Ridzqi Badarudin
+#NIM    : J0403251146
+#Kelas  : B
+#====================================
+
+#====================================
+#Studi kasus : Sistem Antrian Layanan Akademik
+#Implementasi Queue =>
+# Stack ==> Front -> c -> B -> A -> None
+# Front -> A -> B -> C -> Rear
+#====================================
+
+
+
+
+
+
